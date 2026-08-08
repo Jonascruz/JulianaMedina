275285fb6bd333206ee2b807bf016897b7ee8e33
Jonas Nogueira da Cruz <jonascruz04@gmail.com>
2026-08-08 17:52:46 -0300

    Ajusta arquivos ignorados no deploy Hostinger

--- loja/wp-content/plugins/woocommerce-mercadopago/build/basic.block.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities'), 'version' => 'dac8e690ef4f9b83861e');
--- loja/wp-content/plugins/woocommerce-mercadopago/build/pse.block.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities'), 'version' => '781be5ce46d643f71b89');
